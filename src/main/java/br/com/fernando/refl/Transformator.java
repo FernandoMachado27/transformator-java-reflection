bf86b3915260428cc40b8639eb2a9988cd6ee632
Fernando Machado <fernandomachado@MacBook-Neo-de-Fernando.local>
2026-08-04 23:08:09 -0300

    Pegando os campos de uma classe

--- src/main/java/br/com/fernando/refl/Transformator.java
+++ src/main/java/br/com/fernando/refl/Transformator.java
@@ -1,15 +1,20 @@
 package br.com.fernando.refl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Transformator {
 
     public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException { // input -> classe que o suário vai passar
         Class<?> source = input.getClass(); // recebe a classe
-        Class<?> target = Class.forName(source.getName() + "DTO"); // instanciar classe + DTO
+        Class<?> target = Class.forName(source + "DTO"); // instanciar classe + DTO
 
         // busca o controtor da classe xDTO
         O targetClass = (O) target.getDeclaredConstructor().newInstance(); // -> "O" pois será o output
+
+        // retorna todos os campos da classe X
+        Field[] sourcesFields = source.getDeclaredFields();
+        Field[] targetFields = target.getDeclaredFields();
 
         return targetClass;
     }
