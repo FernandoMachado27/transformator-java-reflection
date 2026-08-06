20747147ae81bcc54b57e4c281b8b1d513ced2d6
Fernando Machado <fernandomachado@MacBook-Neo-de-Fernando.local>
2026-08-06 19:15:18 -0300

    Teste shouldTransform

--- src/main/java/br/com/fernando/refl/Transformator.java
+++ src/main/java/br/com/fernando/refl/Transformator.java
@@ -8,7 +8,7 @@ public class Transformator {
 
     public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException { // input -> classe que o suário vai passar
         Class<?> source = input.getClass(); // recebe a classe
-        Class<?> target = Class.forName(source + "DTO"); // instanciar classe + DTO
+        Class<?> target = Class.forName(source.getName() + "DTO"); // instanciar classe + DTO
 
         // busca o construtor da classe xDTO
         O targetClass = (O) target.getDeclaredConstructor().newInstance(); // -> "O" pois será o output
@@ -18,26 +18,30 @@ public class Transformator {
         Field[] targetFields = target.getDeclaredFields();
 
         // forEach nas 2 classes para comparar os campos
-        Arrays.stream(sourcesFields).forEach(sourcesField ->
+        Arrays.stream(sourcesFields).forEach(sourceField ->
                 Arrays.stream(targetFields).forEach(targetField -> {
-                    validate(sourcesField, targetField);
-                    try {
-                        targetField.set(targetClass, sourcesField.get(input)); // preenche no targetClass o que está salvo no campo do input
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
+
+                    if (validate(sourceField, targetField)) {
+                        try {
+                            sourceField.setAccessible(true); // acesso às infos privadas da classe de origem
+                            targetField.setAccessible(true); // acesso às infos privadas da classe de destino
+
+                            // preenche no targetClass o que está salvo no campo do input
+                            targetField.set(targetClass, sourceField.get(input));
+
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                }));
+                })
+        );
 
         return targetClass;
     }
 
     // metodo de comparação
-    private void validate(Field sourceField, Field targetField) {
-        if(sourceField.getName().equals(targetField.getName())
-                && sourceField.getType().equals(targetField.getType())) {
-            sourceField.setAccessible(true); // acesso as infos privadas
-            targetField.setAccessible(true);
-        }
+    private boolean validate(Field sourceField, Field targetField) {
+        return sourceField.getName().equals(targetField.getName())
+                && sourceField.getType().equals(targetField.getType());
     }
-
 }
--- src/test/java/br/com/fernando/refl/TransformatorTest.java
+++ src/test/java/br/com/fernando/refl/TransformatorTest.java
@@ -1,7 +1,25 @@
 package br.com.fernando.refl;
 
+import br.com.fernando.Pessoa;
+import br.com.fernando.PessoaDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class TransformatorTest {
 
+    Pessoa pessoa = new Pessoa(1, "João", "1234");
 
+    @Test
+    public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Transformator transformator = new Transformator();
+        PessoaDTO pessoaDTO = transformator.transform(pessoa);
+
+        Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO); // valida se realmente é uma pessoaDTO
+        Assertions.assertEquals(pessoa.getNome(), pessoaDTO.getNome());
+        Assertions.assertEquals(pessoa.getCpf(), pessoaDTO.getCpf());
+        System.out.println(pessoaDTO.getNome());
+    }
 
 }
